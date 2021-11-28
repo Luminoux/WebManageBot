39c63f41cfefabfd994d36529e8b5c004ede2c66
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2021-11-28 16:11:53 -0600

    Change `ping` command file structure completely

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -4,15 +4,10 @@ module.exports = {
     name: 'ping',
     description: 'Shows my ping',
     run: async (client, message) => {
-
-        const pings = {
-            Reply: `${Date.now() - message.createdTimestamp}ms`,
-            Websocket: `${Math.round(client.ws.ping)}ms`
-        }
-    
         const ping = new MessageEmbed()
             .setTitle('**__PONG! My latencies:__**')
-            .setFields(pings)
+            .addField(`Reply: ${Date.now() - message.createdTimestamp}ms`)
+            .addField(`Websocket: ${Math.round(client.ws.ping)}ms`)
             .setTimestamp()
         
         message.reply({ embeds: [ping] })
