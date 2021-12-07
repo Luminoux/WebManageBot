fbc61106431268b1c4165caeace2def5e75cf338
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2021-12-07 15:31:24 -0600

    Maybe fix `test02` command

--- commands/test/test02.js
+++ commands/test/test02.js
@@ -78,25 +78,8 @@ module.exports = {
     
             categories.push(cats);
           });
-        const cmds = commands.map((command) => {
-          let file = require(`../../commands/${dir}/${command}`);
 
-          if (!file.name) return "No command name.";
-
-          let name = file.name.replace(".js", "");
-
-          let des = `${client.commands.get(name).description}`;
-          let emo = `✅`;
-
-          let obj = {
-            cname: `${emo} \`${name}\``,
-            des,
-          };
-
-          return obj;
-        });
-
-        const help = new MessageEmbed()
+        const helpEmbed = new MessageEmbed()
           .setTitle(`\`\`Help Menu\`\``)
           .setDescription(`\`\`My Prefix is : ${config.prefix} \`\`\n \`\`\` Presented By Luminoux Studios \`\`\` \n To check out a category, use command ${config.prefix}help [category] \n\n [Invite Me Now](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands) \n [My Support Server](https://discord.gg/m5xUeZj7Xb) \n [My Other Server](https://discord.gg/aFCQSyzNU8)`)
           .addFields(categories)
